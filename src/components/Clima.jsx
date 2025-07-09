0225d42aa350a9d8b7938fc758eeba81c7bc561c
javier <javiercastillo.tuc@gmail.com>
2025-07-09 19:40:26 -0300

    arreglando

--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import '../App.css'
 import temperatura from '../assets/temperatura.png';
 import humedad from '../assets/humedad.png';
-import viento from '../assets/viento.webp';
+import viento from '../assets/viento.png';
 import icono from '../assets/icono.png';
 
 
@@ -54,8 +54,8 @@ function Clima(){
         }
     }
 
-    return <main className="bg-white d-flex flex-column align-items-center justify-content-center text-center py-5">
-        <div className="container  bg-body-tertiary sombra py-3">
+    return <main className="bg-white fondo d-flex flex-column align-items-center justify-content-center text-center py-5">
+        <div className="container transparencia sombra py-3">
             
             <div className="d-flex justify-content-center mb-3 gap-3">
                 <h1 className="text-center titulo">Clima App </h1>
@@ -67,37 +67,38 @@ function Clima(){
             
         <form className="d-flex justify-content-center gap-3">
             <input type="text" onChange={valorExtraer} value={inputValor} />
-            <button className="btn btn-primary titulo" onClick={obtenerClima}>Consultar</button>
+            <button className="btn btn-dark titulo" onClick={obtenerClima}>Consultar</button>
         </form>
 
-        <div className=" mb-3">
-
+        {clima.name && (
+            <div className=" mb-3">
+        
             <p className="text-danger mt-3">{error}</p>
             <h3 className="w-100 mb-4">{clima.name}</h3>
        
             <div className="row justify-content-center gy-3">
                 <div className="col-auto">
-                    <div className="card redondeado border-0">
+                    <div className="redondeado border-0">
                         <div className="d-flex flex-column justify-content-center py-3 align-items-center">
-                            <p className="text-danger">Temperatura</p>
+                            <p>Temperatura</p>
                             <img src={temperatura} className="imagen" alt="termometro" />
-                            <p className="text-danger mb-0 mt-3">{clima.main.temp}º</p>
+                            <p className="mb-0 mt-3">{clima.main.temp}º</p>
                         </div>
                     </div>
                 </div>
 
                 <div className="col-auto">
-                    <div className="card redondeado border-0">
+                    <div className="redondeado border-0">
                         <div className="d-flex flex-column justify-content-center py-3 align-items-center">
-                            <p className="text-primary">Humedad</p>
+                            <p>Humedad</p>
                             <img src={humedad} className="imagen" alt="humedad" />
-                            <p className="text-primary mb-0 mt-3">{clima.main.humidity}%</p>
+                            <p className="mb-0 mt-3">{clima.main.humidity}%</p>
                         </div>
                     </div>
                 </div> 
 
                 <div className="col-auto">
-                    <div className="card redondeado border-0">
+                    <div className="redondeado border-0">
                         <div className="d-flex flex-column justify-content-center py-3 align-items-center">
                             <p>Viento</p>
                             <img src={viento} className="imagen" alt="viento" />
@@ -114,6 +115,8 @@ function Clima(){
 
             })}
         </div>
+        )}
+        
             <hr />
             <p className="text-center mb-0">&copy; Desarrollado por: Javier Castillo</p>
         
