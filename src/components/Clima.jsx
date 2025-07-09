ba6d64777e8fa4576a77f87809908edb99c5c3eb
javier <javiercastillo.tuc@gmail.com>
2025-07-09 15:55:38 -0300

    se termina

--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -26,6 +26,7 @@ function Clima(){
             const respuestaFinalClima = await respuestaClima.json()
             setClima(respuestaFinalClima)
             setError("")
+            setInputValor("")
         } else if (respuestaClima.status === 404){
             setError("Ciudad inexistente")
             setClima({
@@ -34,6 +35,7 @@ function Clima(){
                 main: {},
                 wind: {}
             })
+            setInputValor("")
         } else{
             setError("Ocurrio un error inesperado. Intente de nuevo mas tarde.")
             setClima({
@@ -42,16 +44,20 @@ function Clima(){
                 main: {},
                 wind: {}
             })
+            setInputValor("")
         }
     }
 
-    return <div className="bg-white text-center py-5">
-        <div className="container bg-body-tertiary sombra py-3">
+    return <main className="bg-white d-flex flex-column align-items-center justify-content-center text-center py-5">
+        <div className="container  bg-body-tertiary sombra py-3">
+            
             <div className="d-flex justify-content-center mb-3 gap-3">
                 <h1 className="text-center titulo">Clima App </h1>
                 <img src="./src/assets/icono.png" alt="icono" className="icono" />
 
             </div>
+
+            <p className="lead">¡Ingrese una ciudad para obtener su clima!</p>
             
 
         <form className="d-flex justify-content-center gap-3">
@@ -59,13 +65,13 @@ function Clima(){
             <button className="btn btn-primary titulo" onClick={obtenerClima}>Consultar</button>
         </form>
 
-        <div className="container mb-3">
+        <div className=" mb-3">
 
-            <p className="text-danger">{error}</p>
+            <p className="text-danger mt-3">{error}</p>
             <h3 className="w-100 mb-4">{clima.name}</h3>
        
-            <div className="row">
-                <div className="col-4">
+            <div className="row justify-content-center gy-3">
+                <div className="col-auto">
                     <div className="card redondeado border-0">
                         <div className="d-flex flex-column justify-content-center py-3 align-items-center">
                             <img src="./src/assets/temperatura.png" className="imagen" alt="termometro" />
@@ -74,7 +80,7 @@ function Clima(){
                     </div>
                 </div>
 
-                <div className="col-4">
+                <div className="col-auto">
                     <div className="card redondeado border-0">
                         <div className="d-flex flex-column justify-content-center py-3 align-items-center">
                             <img src="./src/assets/humedad.png" className="imagen" alt="termometro" />
@@ -83,7 +89,7 @@ function Clima(){
                     </div>
                 </div> 
 
-                <div className="col-4">
+                <div className="col-auto">
                     <div className="card redondeado border-0">
                         <div className="d-flex flex-column justify-content-center py-3 align-items-center">
                             <img src="./src/assets/viento.webp" className="imagen" alt="termometro" />
@@ -100,9 +106,12 @@ function Clima(){
 
             })}
         </div>
-
-    </div>
+            <hr />
+            <p className="text-center mb-0">&copy; Desarrollado por: Javier Castillo</p>
+        
         </div>
+        
+    </main>
         
 }
 
